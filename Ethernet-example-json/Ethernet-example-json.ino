e302be76c6a0143fe1beeffb132e8ba93b3a829c
Daniel Bougourd <boujois@gmail.com>
2020-05-03 15:09:28 +0100

    initial commit

---
+++ Ethernet-example-json/Ethernet-example-json.ino
@@ -0,0 +1,69 @@
+#include <SPI.h>
+#include <Ethernet.h>
+#include <ArduinoJson.h> // Library AduinoJson version 5 (not 6)
+
+// Enter a MAC address and IP address for your controller below.
+// The IP address will be dependent on your local network:
+byte mac[] = { 0xDE, 0xAD, 0xBD, 0xEF, 0xFE, 0xED };
+IPAddress ip(192,168,86,183);
+EthernetServer server(80);
+
+void setup() {
+  // Open serial communications and wait for port to open:
+  Serial.begin(9600);
+  // start the Ethernet connection and the server:
+  Ethernet.begin(mac, ip);
+  server.begin();
+  Serial.print("server is at ");
+  Serial.println(Ethernet.localIP());
+}
+
+
+void loop() {
+  // listen for incoming clients
+  EthernetClient client = server.available();
+  if (client) {
+    Serial.println("new client");
+    // an http request ends with a blank line
+    boolean currentLineIsBlank = true;
+    while (client.connected()) {
+      if (client.available()) {
+        char c = client.read();
+        Serial.write(c);
+        if (c == '\n' && currentLineIsBlank) {
+          // send a standard http response header
+          client.println("HTTP/1.1 200 OK");
+          client.println("Content-Type: text/json");
+          client.println("Connection: close");
+          client.println();
+          DynamicJsonBuffer jsonBuffer;
+          JsonObject& object = jsonBuffer.createObject();
+          object["software_version"] = "1.0.1";
+          object["sensor_readings"]= jsonBuffer.createObject(); // create a nested object for the readings
+          for (int analogChannel = 0; analogChannel < 6; analogChannel++) {
+            int sensorReading = analogRead(analogChannel);
+            object["sensor_readings"][String(analogChannel)]=sensorReading;
+          }
+          
+          object.prettyPrintTo(Serial);
+          object.prettyPrintTo(client);
+          client.println();
+          break;
+        }
+        if (c == '\n') {
+          // you're starting a new line
+          currentLineIsBlank = true;
+        } 
+        else if (c != '\r') {
+          // you've gotten a character on the current line
+          currentLineIsBlank = false;
+        }
+      }
+    }
+    // give the web browser time to receive the data
+    delay(1);
+    // close the connection:
+    client.stop();
+    Serial.println("client disonnected");
+  }
+}
--- README.md
+++ README.md
@@ -1,1 +1,38 @@
-# arduino-json-webclient
+# Arduino JSON WebServer
+
+## Getting Started
+
+Open `Ethernet-example-json.ino` in Arduino IDE.
+
+Install `ArduinoJson` Library version `5.13.5`
+
+`Tools` -> `Manage Libraries...` -> search for `arduinojson`, then select Version 5.13.5 from the dropdown:
+
+![libraries manager](manage_libraries.png)
+
+Make up a MAC address if using more than one arduino on the same network:
+
+    byte mac[] = { 0xDE, 0xAD, 0xBD, 0xEF, 0xFE, 0xED };
+
+Change the IP Address to one in your local network:
+
+    IPAddress ip(192,168,86,183);
+
+Upload the Sketch to your board.
+
+You can now access either through a browser at: [http://192.168.86.183](http://192.168.86.183) (this IP will be different if you changed it per the code above).  
+
+Or access through terminal/powershell:
+
+    $ curl http://192.168.86.183
+    {
+      "software_version": "1.0.1",
+      "sensor_readings": {
+        "0": 496,
+        "1": 441,
+        "2": 443,
+        "3": 468,
+        "4": 394,
+        "5": 357
+      }
+    }
